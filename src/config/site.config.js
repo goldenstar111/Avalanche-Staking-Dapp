3344f06494b31ae85a05e82b7476c324f5a77a4b
yatsukalbert@outlook.com <87792824+yatsukalbert@users.noreply.github.com>
2022-01-14 05:53:18 +0200

    create project

---
+++ src/config/site.config.js
@@ -0,0 +1,10 @@
+const contactAddress = "0x12896bF73BC456AaBCAD48D0c5662199deF840f8";
+const http = "https://api.avax.network/ext/bc/C/rpc";
+const chainId = 43114;
+const chainHex = '0xA86A';
+export {
+  contactAddress,
+  http,
+  chainId,
+  chainHex
+};
